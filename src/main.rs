d73ca656e42f6fdf288e0c985feb53470de85f91
Bernát Süli <suli.bernat@gmail.com>
2024-05-27 23:04:14 +0200

    v2, multiboard support

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,22 @@ use tiberius::{Client, Query};
 use tokio::net::TcpStream;
 use tokio_util::compat::TokioAsyncWriteCompatExt;
 
+/*
+usage:
+config.db {Serial_NMBR} (BoardsOnPanel)
+
+return values:
+GS - golden sample
+OK {TimesFailed} (TimesMbTested) - Panel OK for testing
+NK {TimesFailed} (TimesMbTested) - Panel NOK for testing
+ER {Error message} - Program error
+*/
+
 static CONFIG: &str = "config.ini";
 static GOLDEN: &str = "golden_samples";
+
 static LIMIT: i32 = 3;
+static LIMIT_2: i32 = 6;
 
 #[derive(Default)]
 struct Config {
@@ -14,54 +27,48 @@ struct Config {
     database: String,
     password: String,
     username: String,
-    port: u16,
-    auth: String,
 }
 
 impl Config {
-    fn read_config(&mut self, path: PathBuf, verbose: bool) {
+    fn read(path: PathBuf) -> anyhow::Result<Config> {
+        let mut c = Config::default();
+
         if let Ok(config) = Ini::load_from_file(path.clone()) {
             if let Some(jvserver) = config.section(Some("JVSERVER")) {
                 // mandatory fields:
                 if let Some(server) = jvserver.get("SERVER") {
-                    self.server = server.to_owned();
+                    c.server = server.to_owned();
                 }
                 if let Some(password) = jvserver.get("PASSWORD") {
-                    self.password = password.to_owned();
+                    c.password = password.to_owned();
                 }
                 if let Some(username) = jvserver.get("USERNAME") {
-                    self.username = username.to_owned();
+                    c.username = username.to_owned();
                 }
-
-                if self.server.is_empty() || self.password.is_empty() || self.username.is_empty() {
-                    panic!("ERR: Missing mandatory fields from configuration file!")
-                }
-
-                // optional:
                 if let Some(database) = jvserver.get("DATABASE") {
-                    self.database = database.to_owned();
+                    c.database = database.to_owned();
                 }
-                if let Some(port) = jvserver.get("PORT") {
-                    if !port.is_empty() {
-                        if let Ok(x) = port.parse::<u16>() {
-                            self.port = x;
-                        } else if verbose {
-                            eprintln!("W: Could not parse port number: {port}");
-                        }
-                    }
-                }
-                if let Some(auth) = jvserver.get("AUTH") {
-                    self.auth = auth.to_owned();
+
+                if c.server.is_empty()
+                    || c.password.is_empty()
+                    || c.username.is_empty()
+                    || c.database.is_empty()
+                {
+                    return Err(anyhow::Error::msg(
+                        "ER: Missing fields from configuration file!",
+                    ));
                 }
             } else {
-                panic!("ERR: Could not find [JVSERVER] field!");
+                return Err(anyhow::Error::msg("ER: Could not find [JVSERVER] field!"));
             }
         } else {
-            panic!(
-                "ERR: Could not read configuration file! [{}]",
+            return Err(anyhow::Error::msg(format!(
+                "ER: Could not read configuration file! [{}]",
                 path.display()
-            );
+            )));
         }
+
+        Ok(c)
     }
 }
 
@@ -75,39 +82,43 @@ fn load_gs_list(path: PathBuf) -> Vec<String> {
     list
 }
 
+fn increment_sn(start: &str, boards: u8) -> Vec<String> {
+    // VLLDDDxxxxxxx*
+    // x is 7 digits -> u32
+    let mut ret = Vec::with_capacity(boards as usize);
+    ret.push(start.to_string());
+
+    let sn = &start[6..13].parse::<u32>().expect("ER: Parsing error");
+
+    for i in 1..boards {
+        let nsn = sn + i as u32;
+        let mut next_sn = start.to_string();
+        next_sn.replace_range(6..13, &format!("{:07}", nsn));
+        ret.push(next_sn);
+    }
+
+    ret
+}
+
 async fn get_count(
     config_db: &String,
     tib_config: tiberius::Config,
     target: &String,
-    verbose: bool,
 ) -> anyhow::Result<i32> {
-    if verbose {
-        println!("I: Trying to establish TCP connection");
-    }
     let tcp = TcpStream::connect(tib_config.get_addr()).await?;
     tcp.set_nodelay(true)?;
 
-    if verbose {
-        println!("I: Trying to establish client connection");
-    }
     let mut client = Client::connect(tib_config, tcp.compat_write()).await?;
 
-    if !config_db.is_empty() {
-        let qtext = format!("USE [{}]", config_db);
-        if verbose {
-            println!("> {}", qtext);
-        }
-        let query = Query::new(qtext);
-        query.execute(&mut client).await?;
-    }
+    let qtext = format!("USE [{}]", config_db);
+    let query = Query::new(qtext);
+    query.execute(&mut client).await?;
 
     let qtext = format!(
         "SELECT COUNT(*) FROM [dbo].[SMT_Test] WHERE [Serial_NMBR] = '{}'",
         target
     );
-    if verbose {
-        println!("> {}", qtext);
-    }
+
     let query = Query::new(qtext);
     let result = query.query(&mut client).await?;
 
@@ -117,63 +128,95 @@ async fn get_count(
         }
     }
 
-    Err(anyhow::Error::msg("Failed conversion!"))
+    Err(anyhow::Error::msg("ER: Failed conversion!"))
+}
+
+async fn get_count_for_mb(
+    config_db: &String,
+    tib_config: tiberius::Config,
+    target: &str,
+    boards: u8,
+) -> anyhow::Result<i32> {
+    let tcp = TcpStream::connect(tib_config.get_addr()).await?;
+    tcp.set_nodelay(true)?;
+
+    let mut client = Client::connect(tib_config, tcp.compat_write()).await?;
+
+    let qtext = format!("USE [{}]", config_db);
+    let query = Query::new(qtext);
+    query.execute(&mut client).await?;
+
+    let targets: Vec<String> = increment_sn(target, boards)
+        .iter()
+        .map(|f| format!("'{f}'"))
+        .collect();
+    let targets_string = targets.join(", ");
+
+    let qtext = format!(
+        "SELECT COUNT(*) AS Fails
+        FROM [dbo].[SMT_Test]
+        WHERE [Serial_NMBR] IN ({})
+        AND [Result] = 'Fail'
+        GROUP BY [Serial_NMBR]
+		ORDER BY Fails DESC;",
+        targets_string
+    );
+
+    let query = Query::new(qtext);
+    let result = query.query(&mut client).await?;
+
+    if let Some(row) = result.into_row().await? {
+        if let Some(x) = row.get::<i32, usize>(0) {
+            return Ok(x);
+        }
+    }
+
+    Err(anyhow::Error::msg("ER: Failed conversion!"))
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // The current working directory will be not the directory of the executable,
     // So we will need to make absolut paths for .\config and .\golden_samples
-    let exe_path = env::current_exe().expect("Can't read the directory of the executable!"); // Shouldn't fail.
+    let exe_path = env::current_exe().expect("ER: Can't read the directory of the executable!"); // Shouldn't fail.
 
     // First argument should be the DMC we want to check
     let args: Vec<String> = env::args().collect();
-    let target = args.get(1).expect("ERR: No argument found!").clone();
+    let target = args.get(1).expect("ER: No argument found!").clone(); // Shouldn't happen
 
-    // Additional command line options:
-    // -v -> verbose | For debugging purposes only
-    let mut verbose = false;
+    let boards: u8;
     if let Some(x) = args.get(2) {
-        if x == "-v" {
-            verbose = true;
-        }
+        boards = x.parse().unwrap_or(1);
+    } else {
+        boards = 1;
     }
 
-    // Check if it is a golden sample, if it is then return OK.
+    // Check if it is a golden sample, if it is then return 'GS'.
     let golden_samples: Vec<String> = load_gs_list(exe_path.with_file_name(GOLDEN));
     if golden_samples.contains(&target) {
-        if verbose {
-            println!("I: Target DMC is a golden sample.");
-        }
-        println!("GS"); // BT-BASIC can read the stdout, tell it that the board is a Golden sample
+        println!("GS: Panel is golden sample");
 
         return Ok(());
     }
 
     // Read configuration
-    let mut config = Config::default();
-    config.read_config(exe_path.with_file_name(CONFIG), verbose);
+    let config = match Config::read(exe_path.with_file_name(CONFIG)) {
+        Ok(c) => c,
+        Err(e) => {
+            println!("{e}");
+            std::process::exit(0)
+        }
+    };
 
     // Tiberius configuartion:
     let mut tib_config = tiberius::Config::new();
 
-    if config.port != 0 {
-        tib_config.port(config.port);
-    }
-
     tib_config.host(config.server);
 
-    if config.auth == "WIN" {
-        tib_config.authentication(tiberius::AuthMethod::windows(
-            config.username,
-            config.password,
-        ));
-    } else {
-        tib_config.authentication(tiberius::AuthMethod::sql_server(
-            config.username,
-            config.password,
-        ));
-    }
+    tib_config.authentication(tiberius::AuthMethod::sql_server(
+        config.username,
+        config.password,
+    ));
 
     // Most likely not needed.
     tib_config.trust_cert();
@@ -181,24 +224,45 @@ async fn main() -> anyhow::Result<()> {
 
     // The connection might fail sometimes, so we will try 3 times:
     let mut tries = 0;
-    let mut result = get_count(&config.database, tib_config.clone(), &target, verbose).await;
+    let mut result = get_count(&config.database, tib_config.clone(), &target).await;
     while tries < 2 && result.is_err() {
-        if verbose {
-            println!("ERR: Failed to connect, retrying! {}/2", tries + 1);
-        }
-        result = get_count(&config.database, tib_config.clone(), &target, verbose).await;
+        result = get_count(&config.database, tib_config.clone(), &target).await;
         tries += 1;
     }
 
-    let x = result?;
-    if verbose {
-        println!("Result received: {x}");
+    if result.is_err() {
+        println!("ER: {}", result.err().unwrap());
+        std::process::exit(0)
     }
 
+    let x = result?;
+
     if x < LIMIT {
-        println!("OK"); // Write to stdout so BT-BASIC can read it
-        Ok(())
+        println!("OK: {x}");
+    } else if x >= LIMIT_2 || boards < 2 {
+        println!("NK: {x}");
     } else {
-        panic!("Target limit reached!")
+        // Get the maximum number of failures on the MB
+        let mut tries = 0;
+        let mut result =
+            get_count_for_mb(&config.database, tib_config.clone(), &target, boards).await;
+        while tries < 2 && result.is_err() {
+            result = get_count_for_mb(&config.database, tib_config.clone(), &target, boards).await;
+            tries += 1;
+        }
+
+        if result.is_err() {
+            println!("ER: {}", result.err().unwrap());
+            std::process::exit(0)
+        }
+
+        let y = result?;
+        if y < LIMIT {
+            println!("OK: {y} ({x})");
+        } else {
+            println!("NK: {y} ({x})");
+        }
     }
+
+    Ok(())
 }
